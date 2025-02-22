aac6bdbcb96cd035bafc62fb99b13ce0a3ecb401
farjadakbar4 <farjadakbar4@gmail.com>
2025-02-22 21:59:25 +0500

    fix: task

--- src/app/(dashboard)/projects/tasks/page.tsx
+++ src/app/(dashboard)/projects/tasks/page.tsx
@@ -9,18 +9,24 @@ import Link from "next/link";
 import { requireUser } from "@/lib/user";
 import { DataTable } from "../../components/table-components/data-table";
 import { priorities, statuses } from "./data/data";
+import { getActiveUsers } from "@/actions/get-users";
+import { getBoards } from "@/actions/projects/get-boards";
+import NewTaskDialog from "../dialogs/NewTask";
 
 const TasksPage = async () => {
   const tasks: any = await getTasks();
   const user = await requireUser();
   const userId = user?.id;
 
+  const users = await getActiveUsers();
+  const boards: any = await getBoards(userId!);
+  
   return (
     <Suspense fallback={<SuspenseLoading />}>
       <div className="flex justify-between items-center">
           <H2Title>All tasks</H2Title>
           <div className="flex gap-3">
-            <Button>New task</Button>
+          <NewTaskDialog users={users} boards={boards} />
             <Button asChild>
               <Link href={`/projects/tasks/${userId}`}>My Tasks</Link>
             </Button>
