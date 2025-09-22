f8efb71eaedc0dae105dec421908e3b45736ac28
farjadakbar4 <farjadakbar4@gmail.com>
2025-09-22 11:38:41 +0500

    update config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 module.exports = {
   reactStrictMode: false,
-
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
     images: {
       remotePatterns: [
         {
